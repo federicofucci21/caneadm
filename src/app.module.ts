22b5dabfff71ecc5127902c64f9725b2ffb8c77c
federicofucci21 <federicofucci@hotmail.com>
2023-01-17 18:25:29 -0300

    adding user get route for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AppService } from './app.service';
       database: 'test',
       models: [],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
