9bd21ffabef1865120ec70ad4740da3c6440c89e
federicofucci21 <federicofucci@hotmail.com>
2023-01-17 22:38:36 -0300

    some routes for test added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get()
   getAllUsers() {
     return 'all users';
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return `user with id: ${id}`;
   }
 }
