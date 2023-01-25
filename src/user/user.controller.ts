35bf505a3ac1a4cd7e6edb9695fd5c22d4ba41ed
federicofucci21 <federicofucci@hotmail.com>
2023-01-25 01:00:06 -0300

    creating users routes, findAll, findById, createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+import { User } from './user.model';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get()
   getAllUsers() {
-    return 'all users';
+    return this.userService.findAll();
   }
   @Get(':id')
   getUserById(@Param('id') id: string) {
-    return `user with id: ${id}`;
+    return this.userService.getById(Number(id));
+  }
+
+  @Post()
+  async createUser(@Body() user: UserDto): Promise<User> {
+    // create a new user and return the newly created user
+    return await this.userService.createUser(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,16 +9,20 @@ export class UserService {
     @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,
   ) {}
 
-  async create(user: UserDto): Promise<User> {
+  async createUser(user: UserDto): Promise<User> {
     return await this.userRepository.create<User>(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.findAll();
   }
 
   async findOneByEmail(email: string): Promise<User> {
     return await this.userRepository.findOne<User>({ where: { email } });
   }
 
-  async findOneById(id: number): Promise<User> {
-    return await this.userRepository.findOne<User>({ where: { id } });
+  async getById(id: number): Promise<User> {
+    return await this.userRepository.findByPk<User>(id);
   }
 }
 // import { Injectable } from '@nestjs/common';
