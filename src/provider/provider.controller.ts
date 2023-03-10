efd3970f9e8c42dadafbfe38c4afc48ad5ea6504
federicofucci21 <federicofucci@hotmail.com>
2023-03-10 00:00:02 -0300

    get, get by id

--- src/provider/provider.controller.ts
+++ src/provider/provider.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
+import { Body, Get, Param, Post } from '@nestjs/common/decorators';
+import { ProviderDTO } from './dto/provider.dto';
+import { ProviderService } from './provider.service';
 
 @Controller('provider')
-export class ProviderController {}
+export class ProviderController {
+  constructor(private readonly providerService: ProviderService) {}
+
+  @Get()
+  public async getAllProviders() {
+    return await this.providerService.findProviders();
+  }
+
+  @Get('id/:id')
+  public async getProviderById(@Param('id') id: string) {
+    return await this.providerService.getById(Number(id));
+  }
+
+  @Post()
+  public async createUser(@Body() body: ProviderDTO) {
+    return await this.providerService.createProvider(body);
+  }
+}
--- src/provider/provider.service.ts
+++ src/provider/provider.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProviderDTO } from './dto/provider.dto';
+import { ProviderEntity } from './entities/provider.entity';
 
 @Injectable()
-export class ProviderService {}
+export class ProviderService {
+  constructor(
+    @InjectRepository(ProviderEntity)
+    private readonly providerRepository: Repository<ProviderEntity>,
+  ) {}
+
+  public async createProvider(body: ProviderDTO): Promise<ProviderEntity> {
+    try {
+      return await this.providerRepository.save(body);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async findProviders(): Promise<ProviderEntity[]> {
+    try {
+      return await this.providerRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async getById(id: number): Promise<ProviderEntity> {
+    try {
+      return await this.providerRepository
+        .createQueryBuilder('providers')
+        .where({ id })
+        .getOne();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
