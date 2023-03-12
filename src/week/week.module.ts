d8432c28cfa33c75a76793a6f6ceb98511877334
Mario Caballero <mario.caballero.948@gmail.com>
2023-03-12 20:47:32 -0300

    add relation between orders and open week

--- src/week/week.module.ts
+++ src/week/week.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WeekEntity } from './entities/week.entity';
 import { WeekController } from './week.controller';
 import { WeekService } from './week.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WeekEntity])],
   controllers: [WeekController],
   providers: [WeekService],
 })
--- src/week/week.service.ts
+++ src/week/week.service.ts
@@ -1,4 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult } from 'typeorm';
+import { WEEKSTATE } from '../constants/weekState';
+import { WeekDTO, WeekUpdateDTO } from './dto/week.dto';
+import { WeekEntity } from './entities/week.entity';
 
-@Injectable()
-export class WeekService {}
+export class WeekService {
+  constructor(
+    @InjectRepository(WeekEntity)
+    private readonly weekRepository: Repository<WeekEntity>,
+  ) {}
+
+  public async allWeeks(): Promise<WeekEntity[]> {
+    try {
+      return await this.weekRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async getWeekById(id: number): Promise<WeekEntity | string> {
+    try {
+      const week = await this.weekRepository
+        .createQueryBuilder('weeks')
+        .where({ id })
+        .leftJoinAndSelect('weeks.order', 'order')
+        .getOne();
+
+      return week
+        ? week
+        : `We don't have a week with identification ${id} on our database`;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async findOpenWeek(): Promise<WeekEntity> {
+    try {
+      return await this.weekRepository
+        .createQueryBuilder('weeks')
+        .where({ status: WEEKSTATE.OPEN })
+        .getOne();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async weekCreate(body: WeekDTO): Promise<WeekEntity | string> {
+    try {
+      const week = await this.weekRepository
+        .createQueryBuilder('weeks')
+        .where({ status: WEEKSTATE.OPEN })
+        .getOne();
+
+      if (week) {
+        return 'You have an open week, please close it';
+      }
+
+      return this.weekRepository.save(body);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async updateWeek(
+    id: number,
+    body: WeekUpdateDTO,
+  ): Promise<UpdateResult | string> {
+    try {
+      const week: UpdateResult = await this.weekRepository.update(id, body);
+
+      if (week.affected === 0) {
+        return "Week doesn't found";
+      }
+
+      return week;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async deleteWeek(id) {
+    try {
+      return await this.weekRepository.delete(id);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
