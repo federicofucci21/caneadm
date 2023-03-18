c137d2436cbf15478df97c2c22c0bf213de7f200
federicofucci21 <federicofucci@hotmail.com>
2023-03-18 19:50:11 -0300

    feature/update stock when orders created or updated

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,9 +5,16 @@ import { OrderController } from './order.controller';
 import { orderProviders } from './order.providers';
 import { OrderEntity } from './entities/order.entity';
 import { ProductsForOrderEntity } from './entities/productOrder.entity';
+import { ProductEntity } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity, ProductsForOrderEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      OrderEntity,
+      ProductsForOrderEntity,
+      ProductEntity,
+    ]),
+  ],
   providers: [OrderService, ...orderProviders],
   controllers: [OrderController],
 })
