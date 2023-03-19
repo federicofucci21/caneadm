23da66a0e814ddd532b8f53cd29359432251dd0f
Federico Eloy Fucci <federicofucci@hotmail.com>
2023-03-19 11:43:18 -0300

    Product error handler (#54)
    * product error handler
    
    * deleted extra file

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,8 +6,10 @@ import {
   Param,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { ProductDTO, ProductUpdateDTO } from './dto/product.dto';
 import { ProductService } from './product.service';
 
@@ -17,30 +19,31 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
-  public async getAllProducts() {
-    return await this.productService.findAll();
+  public async getAllProducts(@Res() res: Response) {
+    return await this.productService.findAll(res);
   }
 
   @Get(':id')
-  async getProductrById(@Param('id') id: string) {
-    return await this.productService.findOneById(Number(id));
+  async getProductrById(@Param('id') id: string, @Res() res: Response) {
+    return await this.productService.findOneById(Number(id), res);
   }
 
   @Post()
-  public async createProduct(@Body() body: ProductDTO) {
-    return await this.productService.createProduct(body);
+  public async createProduct(@Body() body: ProductDTO, @Res() res: Response) {
+    return await this.productService.createProduct(body, res);
   }
 
   @Delete(':id')
-  async deleteProduct(@Param('id') id: string) {
-    return await this.productService.deleteProduct(Number(id));
+  async deleteProduct(@Param('id') id: string, @Res() res: Response) {
+    return await this.productService.deleteProduct(Number(id), res);
   }
 
   @Put(':id')
   async updateProduct(
     @Param('id') id: string,
     @Body() product: ProductUpdateDTO,
+    @Res() res: Response,
   ) {
-    return await this.productService.updateProduct(Number(id), product);
+    return await this.productService.updateProduct(Number(id), product, res);
   }
 }
