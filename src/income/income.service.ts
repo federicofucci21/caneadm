8c721abc5ac0f4948472b859268d9ccdda179a28
federicofucci21 <federicofucci@hotmail.com>
2023-03-23 00:46:49 -0300

    incomes error handler added

--- src/income/income.service.ts
+++ src/income/income.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ErrorManager } from '../helpers/error.manager';
 import { Repository, UpdateResult } from 'typeorm';
 import { WeekService } from '../week/week.service';
 import { IncomeDTO, IncomeUpdateDTO } from './dto/income.dto';
@@ -13,37 +14,50 @@ export class IncomeService {
     private readonly weekService: WeekService,
   ) {}
 
-  public async createIncome(body: IncomeDTO): Promise<IncomeEntity | string> {
+  public async createIncome(body: IncomeDTO): Promise<IncomeEntity> {
     try {
       const weekOpen = await this.weekService.findOpenWeek();
       if (!weekOpen) {
-        return "We don't have an open week, please open one";
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `There aren't open week, please open one`,
+        });
       }
 
-      return await this.incomeRepository.save({
+      const income = await this.incomeRepository.save({
         ...body,
         date: new Date(),
         week: weekOpen,
       });
+      if (!income) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: `income not created`,
+        });
+      }
+      return income;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async findIncomes(): Promise<IncomeEntity[] | string> {
+  public async findIncomes(): Promise<IncomeEntity[]> {
     try {
       const incomes: IncomeEntity[] = await this.incomeRepository.find();
-      if (!incomes.length) {
-        return "We don't have any incomes on database";
+      if (!incomes) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `No incomes on DataBase`,
+        });
       }
 
       return incomes;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async findIncomeById(id: number): Promise<IncomeEntity | string> {
+  public async findIncomeById(id: number): Promise<IncomeEntity> {
     try {
       const income: IncomeEntity = await this.incomeRepository
         .createQueryBuilder('incomes')
@@ -51,12 +65,14 @@ export class IncomeService {
         .getOne();
 
       if (!income) {
-        return `The id incomes number ${id} doesn't exist on database`;
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `income with ID: ${id} do not exist`,
+        });
       }
-
       return income;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
@@ -68,12 +84,15 @@ export class IncomeService {
       const income: UpdateResult = await this.incomeRepository.update(id, body);
 
       if (income.affected === 0) {
-        return `The id incomes number ${id} doesn't exist on database`;
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `The income with identification ${id} doesn't found on database`,
+        });
       }
 
       return await this.findIncomeById(id);
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 }
