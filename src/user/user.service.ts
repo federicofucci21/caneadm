ace2223f2c0877d245003fc8b74fb449e576c314
federicofucci21 <federicofucci@hotmail.com>
2023-03-22 21:48:57 -0300

    fix/user error handler

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,5 @@
 import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Response } from 'express';
-import { HttpStatus } from '@nestjs/common/enums';
 import { UserDTO, UserUpdateDTO } from './dto/user.dto';
 import { UserEntity } from './entities/user.entity';
 import { OrderEntity } from '../order/entities/order.entity';
@@ -26,42 +24,33 @@ export class UserService {
     private readonly productRepository: Repository<ProductEntity>,
   ) {}
 
-  public async createUser(
-    body: UserDTO,
-    res: Response,
-  ): Promise<UserEntity | Response> {
+  public async createUser(body: UserDTO): Promise<UserEntity> {
     try {
       const user: UserEntity = await this.userRepository.save(body);
       if (!user) {
-        return res
-          .status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .header('Created', 'Not Created')
-          .json({ message: `User not created` });
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: `User not created`,
+        });
       }
-      return res
-        .status(HttpStatus.CREATED)
-        .header('Created', 'User Created')
-        .json(user);
+      return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async findUsers(res: Response): Promise<UserEntity[] | Response> {
+  public async findUsers(): Promise<UserEntity[]> {
     try {
       const users: UserEntity[] = await this.userRepository.find();
-      if (!users) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'Users Not Found')
-          .json({ message: `No users on DataBase` });
+      if (!users.length) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `No users on DataBase`,
+        });
       }
-      return res
-        .status(HttpStatus.FOUND)
-        .header('Found', `${users.length} users found on DataBase`)
-        .json(users);
+      return users;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
@@ -85,128 +74,94 @@ export class UserService {
     }
   }
 
-  public async findOneByCell(
-    cell: string,
-    res: Response,
-  ): Promise<UserEntity | Response> {
+  public async findOneByCell(cell: string): Promise<UserEntity> {
     try {
       const user: UserEntity = await this.userRepository
         .createQueryBuilder('users')
         .where({ cell })
         .getOne();
       if (!user) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'Not Found')
-          .json({
-            message: `User with cellphone number: ${cell} do not exist`,
-          });
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `User with cellphone number: ${cell} do not exist`,
+        });
       } else {
-        return res
-          .status(HttpStatus.FOUND)
-          .header('Found', 'User Found')
-          .json(user);
+        return user;
       }
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async getById(
-    id: number,
-    res: Response,
-  ): Promise<UserEntity | Response> {
+  public async getById(id: number): Promise<UserEntity> {
     try {
       const user: UserEntity = await this.userRepository
         .createQueryBuilder('users')
         .where({ id })
         .getOne();
       if (!user) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'Not Found')
-          .json({ message: `User with ID: ${id} do not exist` });
-      } else {
-        return res
-          .status(HttpStatus.FOUND)
-          .header('Found', `User with ID: ${id} found`)
-          .json(user);
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `User with ID: ${id} do not exist`,
+        });
       }
+      return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async deleteUser(
-    id: number,
-    res: Response,
-  ): Promise<UserEntity | Response> {
+  public async deleteUser(id: number): Promise<UserEntity> {
     try {
       const user: UpdateResult = await this.userRepository.update(id, {
         isActive: false,
       });
 
       if (user.affected === 0) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'User not Found')
-          .json({
-            message: `User with identification ${id} doesn't found on database`,
-          });
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `User with identification ${id} doesn't found on database`,
+        });
       }
-      const userDeleted = await this.getById(id, res);
-      return res
-        .status(HttpStatus.OK)
-        .header('Deleted', `User ID: ${id} deleted`)
-        .json(userDeleted);
+      return await this.getById(id);
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async updateUser(
     id: number,
     body: UserUpdateDTO,
-    res: Response,
-  ): Promise<UserEntity | Response> {
+  ): Promise<UserEntity> {
     try {
       const user: UpdateResult = await this.userRepository.update(id, body);
       if (user.affected === 0) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'User Not Found')
-          .json({
-            message: `The user with ID: ${id} doesn't found on database`,
-          });
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `The user with ID: ${id} doesn't found on database`,
+        });
       }
-      const userUpdated: UserEntity | Response = await this.getById(id, res);
-      return res
-        .status(HttpStatus.OK)
-        .header('Updated', `The user ID: ${id} updated`)
-        .json(userUpdated);
+      return await this.getById(id);
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async orderCreate(
     userId: number,
     products: Array<ProductsForOrderDTO>,
-    res: Response,
-  ): Promise<OrderEntity | Response | any> {
+  ): Promise<OrderEntity> {
     try {
       const weekOpen: WeekEntity = await this.weekService.findOpenWeek();
       if (!weekOpen) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'week not found')
-          .json({
-            message: "We don't have an open week, please open one",
-          });
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: "We don't have an open week, please open one",
+        });
       }
 
       const order: OrderDTO = {
-        user: await this.getById(userId, res),
+        user: await this.getById(userId),
         productsForOrder: await this.productOrderRepository.save(products),
         total: totalPrice(products),
         week: weekOpen,
@@ -219,17 +174,13 @@ export class UserService {
         };
         await this.productRepository.update(id, body);
       });
-      const orderSaved = await this.orderRepository.save(order);
-      return res.status(HttpStatus.OK).header('Saved', 'true').json(orderSaved);
+      return await this.orderRepository.save(order);
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async allUserOrders(
-    id: number,
-    res: Response,
-  ): Promise<OrderEntity[] | Response> {
+  public async allUserOrders(id: number): Promise<OrderEntity[]> {
     try {
       const orders: OrderEntity[] = await this.orderRepository
         .createQueryBuilder('orders')
@@ -238,18 +189,14 @@ export class UserService {
         .leftJoinAndSelect('productsForOrder.product', 'product')
         .getMany();
       if (!orders.length) {
-        return res
-          .status(HttpStatus.NOT_FOUND)
-          .header('Found', 'Not Found')
-          .json({ message: `user with id: ${id}, have no orders on database` });
-      } else {
-        return res
-          .status(HttpStatus.FOUND)
-          .header('Found', 'Orders Found')
-          .json(orders);
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `user with id: ${id}, have no orders on database`,
+        });
       }
+      return orders;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 }
