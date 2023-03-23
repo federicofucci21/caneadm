53d42564fb83ffc3341b83ff27522f6009e60d19
Federico Eloy Fucci <federicofucci@hotmail.com>
2023-03-22 23:39:10 -0300

    provider errorHandler added (#61)

--- src/provider/provider.service.ts
+++ src/provider/provider.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ErrorManager } from '../helpers/error.manager';
 import { Repository, UpdateResult } from 'typeorm';
 import { ProviderDTO, ProviderUpdateDTO } from './dto/provider.dto';
 import { ProviderEntity } from './entities/provider.entity';
@@ -13,54 +14,85 @@ export class ProviderService {
 
   public async createProvider(body: ProviderDTO): Promise<ProviderEntity> {
     try {
-      return await this.providerRepository.save(body);
+      const provider = await this.providerRepository.save(body);
+      if (!provider) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: `provider not created`,
+        });
+      }
+      return provider;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async findProviders(): Promise<ProviderEntity[]> {
     try {
-      return await this.providerRepository.find();
+      const providers = await this.providerRepository.find();
+      if (!providers) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `No providers on DataBase`,
+        });
+      }
+      return providers;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async findOneByName(name: string): Promise<ProviderEntity> {
     try {
-      return await this.providerRepository
+      const provider = await this.providerRepository
         .createQueryBuilder('providers')
         .where({ name })
         .getOne();
+      if (!provider) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `Product named: ${name}, do not exist`,
+        });
+      }
+      return provider;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async getById(id: number): Promise<ProviderEntity> {
     try {
-      return await this.providerRepository
+      const provider = await this.providerRepository
         .createQueryBuilder('providers')
         .where({ id })
         .leftJoinAndSelect('providers.expenses', 'expenses')
         .getOne();
+      if (!provider) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `provider with ID: ${id} do not exist`,
+        });
+      }
+      return provider;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async updateProvider(
     id: number,
     body: ProviderUpdateDTO,
-  ): Promise<ProviderEntity | string> {
+  ): Promise<ProviderEntity> {
     try {
       const provider: UpdateResult = await this.providerRepository.update(
         id,
         body,
       );
       if (provider.affected === 0) {
-        return `The provider with identification ${id} doesn't found on database`;
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: `The provider with identification ${id} doesn't found on database`,
+        });
       }
       return await this.getById(id);
     } catch (error) {
