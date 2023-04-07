2d7647aa822c7fe57f1e034da8b0b1b5bf81d7a2
federicofucci21 <federicofucci@hotmail.com>
2023-04-07 20:23:56 -0300

    feature/user controller unit test

--- src/outgo/outgo.controller.spec.ts
+++ src/outgo/outgo.controller.spec.ts
@@ -1,18 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { WeekEntity } from '../week/entities/week.entity';
+import { OutgoDTO } from './dto/outgo.dto';
 import { OutgoController } from './outgo.controller';
+import { OutgoService } from './outgo.service';
 
-xdescribe('OutgoController', () => {
+// import httpMocks from 'node-mocks-http';
+
+describe('OutgoController', () => {
+  const mockOutgoService = {
+    createOutgo: jest.fn().mockImplementation((body: OutgoDTO) => {
+      return {
+        id: 1,
+        ...body,
+      };
+    }),
+    findOutgoes: jest.fn().mockImplementation(() => mockOutgoArray),
+  };
+  const mockOutgoArray: OutgoDTO[] = [
+    {
+      amount: 10,
+      detail: 'hola',
+      date: new Date(),
+      week: new WeekEntity(),
+    },
+    {
+      amount: 10,
+      detail: 'hola',
+      date: new Date(),
+      week: new WeekEntity(),
+    },
+    {
+      amount: 10,
+      detail: 'hola',
+      date: new Date(),
+      week: new WeekEntity(),
+    },
+  ];
+
+  const mockOutgoPost = {
+    amount: 10,
+    detail: 'hola',
+    date: new Date(),
+    week: new WeekEntity(),
+  };
+
   let controller: OutgoController;
+  // let service: OutgoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       controllers: [OutgoController],
-    }).compile();
+      providers: [OutgoService],
+    })
+      .overrideProvider(OutgoService)
+      .useValue(mockOutgoService)
+      .compile();
 
     controller = module.get<OutgoController>(OutgoController);
+    // service = module.get<OutgoService>(OutgoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should create an outgo', async () => {
+    expect(await controller.createOutgo(mockOutgoPost)).toEqual({
+      id: expect.any(Number),
+      ...mockOutgoPost,
+    });
+  });
+  it('should find All outgo', async () => {
+    expect(await controller.getAllOurgoes()).toHaveLength(3);
   });
 });
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,30 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { orderProviders } from '../order/order.providers';
-import { OrderService } from '../order/order.service';
 import { UserController } from './user.controller';
-import { userProviders } from './user.providers';
 import { UserService } from './user.service';
-import { productProviders } from '../product/product.provider';
+import { WeekEntity } from '../week/entities/week.entity';
+import {
+  mockProductsArray,
+  mockUserPost,
+  mockUserService,
+} from './__mock__/mockUser.controller';
 
-xdescribe('UserController', () => {
+describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [
-        UserService,
-        OrderService,
-        ...userProviders,
-        ...orderProviders,
-        ...productProviders,
-      ],
-    }).compile();
+      providers: [UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<UserController>(UserController);
   });
 
-  xit('should be defined', () => {
+  it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should create a User', async () => {
+    expect(await controller.createUser(mockUserPost)).toEqual({
+      id: expect.any(Number),
+      ...mockUserPost,
+    });
+    expect(mockUserService.createUser).toHaveBeenCalledWith(mockUserPost);
+  });
+  it('should get all users', async () => {
+    expect(await controller.getAllUsers()).toHaveLength(3);
+    expect(mockUserService.findUsers).toHaveBeenCalled();
+  });
+  it('should return one user by id', async () => {
+    expect(await controller.getUserById('1')).toEqual({
+      id: expect.any(Number),
+      ...mockUserPost,
+    });
+    expect(mockUserService.getById).toHaveBeenCalled();
+  });
+  it('should find user by cell', async () => {
+    expect(await controller.getUserByCell('3416745943')).toEqual({
+      id: expect.any(Number),
+      cell: '3416745943',
+      ...mockUserPost,
+    });
+    expect(mockUserService.findOneByCell).toHaveBeenCalled();
+  });
+  it('should return update user', async () => {
+    expect(await controller.editUser('1', mockUserPost)).toEqual({
+      id: expect.any(Number),
+      ...mockUserPost,
+    });
+    expect(mockUserService.updateUser).toHaveBeenCalledWith(
+      expect.any(Number),
+      mockUserPost,
+    );
+  });
+  it('should soft delete a User', async () => {
+    expect(await controller.deleteUser('1')).toEqual({
+      id: expect.any(Number),
+      isActive: false,
+      ...mockUserPost,
+    });
+    expect(mockUserService.deleteUser).toHaveBeenCalledWith(expect.any(Number));
+  });
+  it('should create an Order', async () => {
+    expect(await controller.orderCreate('1', mockProductsArray)).toEqual({
+      total: 100,
+      user: { id: 1, ...mockUserPost },
+      productsForOrder: mockProductsArray,
+      week: new WeekEntity(),
+    });
   });
 });
