4f4e568dc572d184348e533da6184559e33e8aa2
federicofucci21 <federicofucci@hotmail.com>
2023-04-19 19:34:07 -0300

    test/orderController/FindById

--- src/order/test/order.controller.spec.ts
+++ src/order/test/order.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from '../order.controller';
 import { OrderService } from '../order.service';
 import {
+  mockOrder,
   mockOrderService,
   mockOrdersArray,
 } from '../__mock__/mockOrder.controller';
@@ -29,5 +30,13 @@ describe('OrderController', () => {
     expect(await controller.getAllOrders()).toEqual(mockOrdersArray);
     expect(await controller.getAllOrders()).toHaveLength(1);
     expect(mockOrderService.allOrders).toHaveBeenCalled();
+  });
+  // Get Order by ID
+  it('should get an order by ID', async () => {
+    expect(await controller.getOrderById('1')).toEqual(mockOrder);
+    expect(mockOrderService.findOneById).toHaveBeenCalled();
+    expect(mockOrderService.findOneById).toHaveBeenCalledWith(
+      expect.any(Number),
+    );
   });
 });
